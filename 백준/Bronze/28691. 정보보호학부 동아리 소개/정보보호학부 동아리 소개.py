0e1b539cf3944de8a344be66feefea687e3a5587
jelly <sohyunzzq@naver.com>
2024-07-18 03:26:00 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+str1 = input()
+if str1 == "M":
+	print("MatKor")
+elif str1 == "W":
+	print("WiCys")
+elif str1 == "C":
+	print("CyKor")
+elif str1 == "A":
+	print("AlKor")
+elif str1 == "$":
+	print("$clear")
