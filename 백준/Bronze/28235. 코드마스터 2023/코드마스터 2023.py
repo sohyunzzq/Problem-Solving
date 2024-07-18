a2150c07f0c8f38baa7de3b267b13ff005b7a48b
jelly <sohyunzzq@naver.com>
2024-07-18 23:04:24 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+str1 = input()
+if str1 == "SONGDO":
+	print("HIGHSCHOOL")
+elif str1 == "CODE":
+	print("MASTER")
+elif str1 == "2023":
+	print("0611")
+elif str1 == "ALGORITHM":
+	print("CONTEST")
