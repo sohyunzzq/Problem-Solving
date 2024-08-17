196111b52d84e1d8612384de0cc1e1034c1fa4d8
jelly <sohyunzzq@naver.com>
2024-08-18 00:55:24 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,33 +1,8 @@
 a, b, v = map(int, input().split())
 
 v -= a
-ans = 1
 
 if v % (a-b) == 0:
-	print(v//(a-b) + ans)
+	print(v//(a-b) + 1)
 else:
-	print(v//(a-b) +1 +ans)
-
-'''
-2 1 4
-
-2
-
-하루에 올라가는 높이 a - b
-2
--1+2
--1+2
-
-2+1+1
-///
-
-18
-5+4+4+4+4
-5
-
-13
-4+4+4+4
-
-13/4
-
-'''
+	print(v//(a-b) + 2)
