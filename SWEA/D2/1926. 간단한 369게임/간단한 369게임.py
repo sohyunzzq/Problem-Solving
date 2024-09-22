d3b460b5ff2595543dccc7cbbe1566f852640c44
jelly <sohyunzzq@naver.com>
2024-09-22 12:19:12 +0900

    [D2] Title: 간단한 369게임, Time: 109 ms, Memory: 44,776 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,17 +1,13 @@
 def clap(num):
-	if num < 10:
-		if num == 3 or num == 6 or num == 9:
-			print("-", end = " ")
-			return True
-		return False
-	
 	num = str(num)
 	cnt = 0
 	for i in range(len(num)):
 		if num[i] in "369":
 			cnt += 1
+
 	if cnt == 0:
 		return False
+
 	for i in range(cnt):
 		print("-", end = "")
 	print("", end = " ")
