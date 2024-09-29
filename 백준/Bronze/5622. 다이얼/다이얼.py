e24c2f3d85131a94228996c93361ce7b19d129ff
jelly <sohyunzzq@naver.com>
2024-09-30 02:08:44 +0900

    [Bronze II] Title: 다이얼, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,8 @@
+dict1 = {"A": 3, "B": 3, "C": 3, "D": 4, "E": 4, "F": 4, "G": 5, "H": 5, "I": 5, "J": 6, "K": 6, "L": 6, "M": 7, "N": 7, "O": 7, "P": 8, "Q": 8, "R": 8, "S": 8, "T": 9, "U": 9, "V": 9, "W": 10, "X": 10, "Y": 10, "Z": 10}
+
+string = input()
+ans = 0
+for item in string:
+    ans += dict1[item]
+
+print(ans)
