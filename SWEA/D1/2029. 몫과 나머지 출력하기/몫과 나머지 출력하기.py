a73cf85d522325432be9f8bd24d6288fb332d598
jelly <sohyunzzq@naver.com>
2025-04-18 20:42:20 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 96 ms, Memory: 44,764 KB -BaekjoonHub

--- SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.py
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.py
@@ -1,4 +1,8 @@
-T = int(input())
-for i in range(T):
-    lst = list(map(int, input().split()))
-    print("#%s %s %s" % (i+1, lst[0] // lst[1], lst[0] % lst[1]))
+t = int(input())
+
+for tc in range(1, t+1):
+
+    a, b = map(int, input().split())
+
+    print("#{} ".format(tc), end = "")
+    print(int(a/b), a%b)
