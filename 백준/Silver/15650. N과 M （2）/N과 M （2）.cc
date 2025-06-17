a208138f183a369fe4331d9a88fb640ff130041e
jelly <sohyunzzq@naver.com>
2025-06-17 20:32:01 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define endl '\n'
+#define N_SZ 8
+
+int n, m;
+vector<int> num;
+int path[N_SZ + 1];
+
+void func(int curr) {
+	if (curr == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[i] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (path[i] == 0 &&
+			(curr == 0 || num[curr - 1] < i)) {
+			num.push_back(i);
+			path[i] = 1;
+
+			func(curr + 1);
+
+			num.pop_back();
+			path[i] = 0;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	func(0);
+}
