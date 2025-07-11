cc310df169817196a083fe2183c4dd75078f7eb6
sohyun <sohyunzzq@naver.com>
2025-07-12 01:33:20 +0900

    [Bronze I] Title: Fractions are better when continued, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32437. Fractions are better when continued/Fractions are better when continued.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(0); cin.tie(0);
+
+#define N_SZ 40
+
+long long ans[N_SZ];
+
+int main() {
+	fastio;
+
+	int n;
+	cin >> n;
+
+	ans[0] = ans[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+		ans[i] = ans[i - 1] + ans[i - 2];
+
+	cout << ans[n];
+}
