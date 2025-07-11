cc803eaf6567fe5ce7c1ce9a8acd67106ab8b7ad
sohyun <sohyunzzq@naver.com>
2025-07-12 00:54:56 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(0); cin.tie(0);
+
+#define N_SZ 91
+
+long long ans[N_SZ];
+
+int main() {
+	int n;
+	cin >> n;
+
+	ans[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+		ans[i] = ans[i - 1] + ans[i - 2];
+	
+	cout << ans[n];
+}
