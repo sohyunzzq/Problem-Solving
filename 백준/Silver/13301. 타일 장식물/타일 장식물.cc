68f54ad365d4428af1075855ce8aa68ba91ddbaa
sohyun <sohyunzzq@naver.com>
2025-07-12 23:27:42 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2020 KB, Score: 21 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(0); cin.tie(0);
+
+#define N_SZ 27
+
+int main() {
+	int n;
+	cin >> n;
+
+	long long DP[N_SZ];
+
+	DP[1] = 4;
+	DP[2] = 6;
+
+	for (int i = 3; i <= n; i++)
+		DP[i] = DP[i - 1] + DP[i - 2];
+
+	cout << DP[n];
+}
