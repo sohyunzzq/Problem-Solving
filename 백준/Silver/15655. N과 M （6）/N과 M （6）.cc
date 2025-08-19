14cb1e2c3f30854f054187770ffc8a70baee7395
jelly <sohyunzzq@naver.com>
2025-08-20 00:56:00 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+
+#define SZ 8
+
+int n, m;
+vector<int> nums;
+vector<int> selected;
+
+void func(int curr) {
+	if (selected.size() == m) {
+		for (int i = 0; i < m; i++)
+			cout << selected[i] << " ";
+		cout << endl;
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (selected.size() == 0 || selected[curr-1] < nums[i]) {
+			selected.push_back(nums[i]);
+			func(curr + 1);
+			selected.pop_back();
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n >> m;
+	nums.resize(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> nums[i];
+	
+	sort(nums.begin(), nums.end());
+
+	func(0);
+}
