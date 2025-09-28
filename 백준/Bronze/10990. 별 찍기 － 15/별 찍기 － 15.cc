b87f4fb783e849e0f1bbdf3dc25295ba0b781569
jelly <sohyunzzq@naver.com>
2025-09-29 01:20:49 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++)
+			cout << " ";
+		cout << "*";
+		for (int j = 0; j < i; j++)
+			cout << " ";
+
+		for (int j = 0; j < i - 1; j++)
+			cout << " ";
+
+		if (i != 0)
+			cout << "*";
+
+		cout << endl;
+	}
+}
