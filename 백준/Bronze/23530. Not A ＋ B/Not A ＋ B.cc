19ace5bfc0a01c47be008445605eaaec22907a77
jelly <sohyunzzq@naver.com>
2025-10-21 02:56:49 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << 1 << endl;
+	}
+}
