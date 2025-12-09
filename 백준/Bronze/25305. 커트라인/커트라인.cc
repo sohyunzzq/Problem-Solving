7deaa0bde4c3d13ff292ad8935489b29f715fb22
jelly <sohyunzzq@naver.com>
2025-12-10 04:32:54 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr.begin(), arr.end(), greater<>());
+
+	cout << arr[k - 1];
+}
