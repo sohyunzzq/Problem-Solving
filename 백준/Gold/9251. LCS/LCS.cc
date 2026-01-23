e19f7f9a4f50f2ad7156e1631064f9c12240f878
jelly <sohyunzzq@naver.com>
2026-01-23 17:19:12 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 6000 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -5,43 +5,43 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 
-	string s1, s2;
-	cin >> s1 >> s2;
+	string A, B;
+	cin >> A >> B;
 
-	int len1 = s1.size();
-	int len2 = s2.size();
+	int A_len = A.size();
+	int B_len = B.size();
 
 	vector<vector<int>> DP;
-	DP.resize(len1);
-	for (int i = 0; i < len1; i++)
-		DP[i].resize(len2);
+	DP.resize(A_len);
+	for (int i = 0; i < A_len; i++)
+		DP[i].resize(B_len);
 
-	DP[0][0] = (s1[0] == s2[0]);
+	DP[0][0] = (A[0] == B[0]);
 	
 	// 0행
-	for (int i = 1; i < len2; i++) {
-		DP[0][i] = DP[0][i - 1];
-		if (!DP[0][i] && s1[0] == s2[i])
+	for (int i = 1; i < B_len; i++) {
+		if (A[0] == B[i])
 			DP[0][i] = 1;
+		else
+			DP[0][i] = DP[0][i - 1];
 	}
 
 	// 0열
-	for (int i = 1; i < len1; i++) {
-		DP[i][0] = DP[i-1][0];
-		if (!DP[i][0] && s1[i] == s2[0])
+	for (int i = 1; i < A_len; i++) {
+		if (A[i] == B[0])
 			DP[i][0] = 1;
+		else
+			DP[i][0] = DP[i - 1][0];
 	}
 
-	for (int i = 1; i < len1; i++) {
-		for (int j = 1; j < len2; j++) {
-			if (s1[i] == s2[j]) {
+	for (int i = 1; i < A_len; i++) {
+		for (int j = 1; j < B_len; j++) {
+			if (A[i] == B[j])
 				DP[i][j] = DP[i - 1][j - 1] + 1;
-			}
-			else {
+			else 
 				DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
-			}
 		}
 	}
 
-	cout << DP[len1 - 1][len2 - 1];
+	cout << DP[A_len - 1][B_len - 1];
 }
