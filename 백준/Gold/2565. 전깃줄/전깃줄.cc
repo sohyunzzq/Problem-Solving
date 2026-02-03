2c98020423a74a9c5f76379fecc1dbdd1a1a2ebc
jelly <sohyunzzq@naver.com>
2026-02-03 23:16:45 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -13,33 +13,28 @@ int n;
 vector<pair<int, int>> arr;
 vector<int> LIS;
 
-void BinarySearch(int left, int right, int target) {
+int BinarySearch(int left, int right, int target) {
 	// lowerbound 찾기
-	while (left <= right) {
+	while (left < right) {
 		int mid = (left + right) / 2;
 
-		if (LIS[mid] > target) {
-			right = mid - 1;
-		}
-		else {
+		if (LIS[mid] < target)
 			left = mid + 1;
-		}
-
+		else
+			right = mid;
 	}
-	if (LIS[left] < target)
-		LIS[left + 1] = target;
-	else
-		LIS[left] = target;
+
+	return left;
 }
 
 void GetLIS() {
 	LIS.push_back(arr[0].second);
 	for (int i = 1; i < n; i++) {
-		if (arr[i].second > LIS[LIS.size() - 1]) {
+		if (arr[i].second > LIS[LIS.size() - 1])
 			LIS.push_back(arr[i].second);
-		}
 		else {
-			BinarySearch(0, LIS.size() - 1, arr[i].second);
+			int tmp = BinarySearch(0, LIS.size() - 1, arr[i].second);
+			LIS[tmp] = arr[i].second;
 		}
 	}
 }
