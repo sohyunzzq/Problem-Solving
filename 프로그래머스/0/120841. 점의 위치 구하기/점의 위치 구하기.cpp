fca90741bcc7958fb8726b901090a9ed4fe2f4cf
jelly <sohyunzzq@naver.com>
2026-07-07 02:02:24 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+
+int solution(vector<int> dot) {
+    if (dot[0] > 0 && dot[1] > 0)
+        return 1;
+    if (dot[0] < 0 && dot[1] > 0)
+        return 2;
+    if (dot[0] < 0 && dot[1] < 0)
+        return 3;
+    return 4;
+}
