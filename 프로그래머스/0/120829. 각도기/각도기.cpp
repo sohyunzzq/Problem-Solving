40d47b3fdcc1ce4b26ef4e01ced095d4be6f71c2
jelly <sohyunzzq@naver.com>
2026-07-06 23:24:15 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+int solution(int angle) {
+    if (angle < 90)
+        return 1;
+    if (angle == 90)
+        return 2;
+    if (angle < 180)
+        return 3;
+    return 4;
+}
