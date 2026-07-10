0cd31eb2d0a3515c7049a748f465e52c685f6c21
jelly <sohyunzzq@naver.com>
2026-07-10 15:43:08 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+string solution(string my_string, int k) {
+    string answer = "";
+    
+    for (int i=0; i<k; i++)
+        answer += my_string;
+    
+    return answer;
+}
