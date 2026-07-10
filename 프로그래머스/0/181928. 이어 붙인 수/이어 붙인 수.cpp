3dee12164a9e278abd237dd2ef98e6c4cb2e0fcc
jelly <sohyunzzq@naver.com>
2026-07-10 15:55:40 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int odd = 0;
+    int even = 0;
+    
+    for (int i=0; i<num_list.size(); i++)
+        if (num_list[i] % 2 == 0) {
+            even *= 10;
+            even += num_list[i];
+        }
+        else {
+            odd *= 10;
+            odd += num_list[i];
+        }
+    return odd + even;
+}
